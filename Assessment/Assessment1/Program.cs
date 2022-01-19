cfb1476b079d5a5069b20bc46a483fced34b76fc
mAnct <97579001+mAnct@users.noreply.github.com>
2022-01-19 09:12:13 +0800

    Assessment #1.

---
+++ Assessment/Assessment1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assessment1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = Enumerable.Range(1, 100).ToArray();
+
+            Random rand = new Random();
+            
+            int index = rand.Next(numbers.Length);
+            var notSorted = numbers;
+            Array.Sort(numbers);
+
+            Console.WriteLine($"Randomly removed number in the array (sorted) is: {numbers[index]}");
+            Console.WriteLine($"Randomly removed number in the array (not sorted) is: {notSorted[index]}");
+            Console.ReadKey();
+
+        }
+    }
+}
